a4ea1edf9b6490969db95b19f36554d336530169
saketh-24 <saketh1094@gmail.com>
2024-06-26 20:47:26 +0530

    User Credentials Saved in db

--- Backend/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = ()=>
-    {
-        mongoose.connect('mongodb://localhost:27017/JWT-Auth')
-        .then(console.log("db connected successfully......."))
-        .catch((error)=>{console.log(error)})
-    }
